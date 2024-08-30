2fdb97f84261d69a24b12f7c69306c97ba476a93
DannyK05 <olubanjok@gmail.com>
2024-08-30 14:45:07 +0100

    feat: implement responsiveness for mobile and tablet

--- src/pages/auth/components/AuthForm.jsx
+++ src/pages/auth/components/AuthForm.jsx
@@ -7,9 +7,9 @@ export const AuthForm = ({ type }) => {
     <div
       className={`${
         type === "signup" ? "flex-row-reverse" : ""
-      } flex w-3/4  h-[80vh] items-center shadow-md `}
+      } flex w-full h-full md:w-3/4 lg:w-3/4 md:h-[80%] lg:h-[80%] items-center shadow-md `}
     >
-      <div className="w-2/5 h-full rounded-l-lg flex items-center flex-col space-y-10 text-center text-lost-white p-10 bg-lost-blue">
+      <div className="w-2/5 hidden md:block lg:block h-full rounded-l-lg flex items-center flex-col space-y-10 text-center text-lost-white p-10 bg-lost-blue">
         {type === "signup" ? (
           <>
             <div className="flex flex-col items-center text-lost-white space-y-4">
@@ -49,21 +49,26 @@ export const AuthForm = ({ type }) => {
           </>
         )}
       </div>
-      <div className="w-3/5 h-full flex flex-col items-center py-10 space-y-6 rounded-r-lg p-5 bg-lost-white">
+      <div className="w-full lg:w-3/5 md:w-3/5 h-full flex flex-col items-center py-10 space-y-6 rounded-r-lg p-5 bg-lost-white">
         {type === "signup" ? (
           <>
+            <img
+              className="md:hidden lg:hidden w-2/3"
+              src={SignupIllustration}
+              attributeName="Image by storyset on Freepik"
+            />
             <h1 className="text-2xl text-lost-blue mb-6 font-bold">
               Sign Up with Us
             </h1>
             <form className="flex w-[60%] flex-col items-center space-y-4">
               <div className="flex w-full flex-col items-center space-y-4">
-                <div className="flex items-center w-full space-x-4">
+                <div className=" flex flex-col md:flex-row lg:flex-row items-center w-full space-y-4 md:space-x-4 lg:space-y-0 md:space-y-0 lg:space-x-4">
                   <FormInput
-                    containerClassName="w-[45%]"
+                    containerClassName="w-full md:w-[45%] lg:w-[45%]"
                     placeholder={"First Name"}
                   />
                   <FormInput
-                    containerClassName="w-[45%]"
+                    containerClassName="w-full md:w-[45%] lg:w-[45%]"
                     placeholder={"Last Name"}
                   />
                 </div>
@@ -83,30 +88,38 @@ export const AuthForm = ({ type }) => {
                   placeholder={"Confirm Password"}
                 />
               </div>
-              <FormButton className="w-[50%]">Sign Up</FormButton>
+              <FormButton className="w-full md:w-[50%] lg:w-[50%]">
+                Sign Up
+              </FormButton>
             </form>
           </>
         ) : (
           <>
-            {" "}
+            <img
+              className="md:hidden lg:hidden w-2/3"
+              src={LoginIllustration}
+              attributeName="Image by storyset on Freepik"
+            />
             <h1 className="text-2xl text-lost-blue mb-6 font-bold">
               Sign In to Lost&Found Hub
             </h1>
             <form className="flex w-full flex-col items-center mb-10 space-y-10">
               <div className="flex w-full flex-col items-center space-y-4">
                 <FormInput
-                  containerClassName="w-[60%]"
+                  containerClassName="w-full md:w-[60%] lg:w-[60%]"
                   type={"email"}
                   placeholder={"Email Address"}
                 />
                 <FormInput
-                  containerClassName="w-[60%]"
+                  containerClassName="w-full md:w-[60%] lg:w-[60%]"
                   type="password"
                   placeholder={"Password"}
                 />
               </div>
 
-              <FormButton className="w-1/3">Sign In</FormButton>
+              <FormButton className="w-full md:w-1/3 lg:w-1/3">
+                Sign In
+              </FormButton>
             </form>
             <span className="text-lost-accent-dark text-sm cursor-pointer hover:underline mt-10">
               Forgot Password ?
--- src/pages/auth/signup/index.jsx
+++ src/pages/auth/signup/index.jsx
@@ -2,7 +2,7 @@ import { AuthForm } from "../components/AuthForm";
 
 export const Signup = () => {
   return (
-    <div className="flex h-full items-center justify-center">
+    <div className="md:flex lg:flex h-full items-center justify-center">
       <AuthForm type={"signup"} />{" "}
     </div>
   );
